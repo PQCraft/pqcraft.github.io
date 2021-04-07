9433962b49f55160539f71b78194ecdefdba2cbc
PQCraft <58464017+PQCraft@users.noreply.github.com>
2021-04-07 10:11:53 -0400

    Add files via upload

--- js/pageload.js
+++ js/pageload.js
@@ -1,2 +1,3 @@
+$('#noload').replaceWith('<div></div>');
 $('#title').replaceWith("<title>PQCraft\'s Github.io Page: " + name + "</title>");
 $('#pgname').replaceWith('<div class="subheader"><b><a class="normcur hlink" style="font-size: 24px; color: white" HREF="" style="color: white">> ' + name + '</a></b></div>');
