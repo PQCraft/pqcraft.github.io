838d30f5bfafe2967523922ec4a204f73c380857
PQCraft <58464017+PQCraft@users.noreply.github.com>
2021-04-08 08:39:12 -0400

    Fixed a lot of things

--- js/pageload.js
+++ js/pageload.js
@@ -1,12 +1,14 @@
 $('#noload').replaceWith('<div></div>');
 function pgload() {
     //console.log("name: {" + name +"}\n");
+    var divprop1 = '<div id="pgname" class="subheader" style="padding: 0px 14px; height: 34px;"><b><a class="normcur hlink" style="font-size: 18px; color: white;" HREF="" style="color: white">';
+    var divprop2 = '</a></b></div>'
     if (name != '') {
         $('#title').replaceWith('<title id="title">PQCraft\'s Github.io Page: ' + name + '</title>');
-        $('#pgname').replaceWith('<div id="pgname" class="subheader"><b><a class="normcur hlink" style="font-size: 24px; color: white; padding: 0px 10px" HREF="" style="color: white">>&nbsp;&nbsp;' + name + '</a></b></div>');
+        $('#pgname').replaceWith(divprop1 + '>&nbsp;&nbsp;' + name + divprop2);
     } else {
         $('#title').replaceWith('<title id="title">PQCraft\'s Github.io Page');
-        $('#pgname').replaceWith('<div id="pgname" class="subheader"><b><a class="normcur hlink" style="font-size: 24px; color: white" HREF="" style="color: white"><br></a></b></div>');
+        $('#pgname').replaceWith(divprop1 + '<br>' + divprop2);
     }
 }
 pgload();
