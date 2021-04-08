55c22b03c89d507531ae646e61a37b12cdf4ce49
PQCraft <58464017+PQCraft@users.noreply.github.com>
2021-04-07 20:44:23 -0400

    Cleaned up layout

--- js/pageload.js
+++ js/pageload.js
@@ -3,7 +3,7 @@ function pgload() {
     //console.log("name: {" + name +"}\n");
     if (name != '') {
         $('#title').replaceWith('<title id="title">PQCraft\'s Github.io Page: ' + name + '</title>');
-        $('#pgname').replaceWith('<div id="pgname" class="subheader"><b><a class="normcur hlink" style="font-size: 24px; color: white" HREF="" style="color: white">> ' + name + '</a></b></div>');
+        $('#pgname').replaceWith('<div id="pgname" class="subheader"><b><a class="normcur hlink" style="font-size: 24px; color: white; padding: 0px 10px" HREF="" style="color: white">>&nbsp;&nbsp;' + name + '</a></b></div>');
     } else {
         $('#title').replaceWith('<title id="title">PQCraft\'s Github.io Page');
         $('#pgname').replaceWith('<div id="pgname" class="subheader"><b><a class="normcur hlink" style="font-size: 24px; color: white" HREF="" style="color: white"><br></a></b></div>');
