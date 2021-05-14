3e0d32db08a994f9a2970f31503a74e46f1888d4
PQCraft <58464017+PQCraft@users.noreply.github.com>
2021-05-14 13:55:47 -0400

    Add files via upload

--- js/pageload.js
+++ js/pageload.js
@@ -1,15 +1,14 @@
-$('#noload').replaceWith('<div></div>');
 function pgload() {
-    //console.log("name: {" + name +"}\n");
     var divprop1 = '<div id="pgname" style="display: block"><div class="subheader" style="height: 32px;"><b><a class="normcur hlink" style="font-size: 18px; color: var(--font-color);" HREF="">';
     var divprop2 = '</a></b></div><br><br></div>'
     if (name != '') {
-        $('#title').replaceWith('<title id="title">PQCraft\'s Github.io Page: ' + name + '</title>');
-        $('#pgname').replaceWith(divprop1 + '>&nbsp;&nbsp;' + name + divprop2);
+        document.getElementById("title").outerHTML = '<title id="title">PQCraft\'s Github.io Page: ' + name + '</title>';
+        document.getElementById("pgname").outerHTML = divprop1 + '>&nbsp;&nbsp;' + name + divprop2;
     } else {
-        $('#title').replaceWith('<title id="title">PQCraft\'s Github.io Page</title>');
-        $('#pgname').replaceWith(divprop1 + '<br>' + divprop2);
+        document.getElementById("title").outerHTML = '<title id="title">PQCraft\'s Github.io Page</title>';
+        document.getElementById("pgname").outerHTML = divprop1 + '<br>' + divprop2;
     }
+    if (typeof disableTab === 'undefined' || disableTab != true) {document.getElementsByTagName('body')[0].outerHTML = document.getElementsByTagName('body')[0].outerHTML.replace(/<tb>/g, "&nbsp;&nbsp;&nbsp;&nbsp;");}
+    document.getElementById("noload").outerHTML = '';
 }
 pgload();
-document.documentElement.innerHTML = document.documentElement.innerHTML.replace(/<tb>/g, "&nbsp;&nbsp;&nbsp;&nbsp;");
