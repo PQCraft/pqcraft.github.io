e3864186718408d5f2d87e35f8269e49f1dadbbd
PQCraft <58464017+PQCraft@users.noreply.github.com>
2021-05-14 11:02:34 -0400

    Add files via upload

--- js/pageload.js
+++ js/pageload.js
@@ -1,7 +1,7 @@
 $('#noload').replaceWith('<div></div>');
 function pgload() {
     //console.log("name: {" + name +"}\n");
-    var divprop1 = '<div id="pgname" style="display: block"><div class="subheader" style="height: 32px;"><b><a class="normcur hlink" style="font-size: 18px; color: #fdfdfd;" HREF="">';
+    var divprop1 = '<div id="pgname" style="display: block"><div class="subheader" style="height: 32px;"><b><a class="normcur hlink" style="font-size: 18px; color: var(--font-color);" HREF="">';
     var divprop2 = '</a></b></div><br><br></div>'
     if (name != '') {
         $('#title').replaceWith('<title id="title">PQCraft\'s Github.io Page: ' + name + '</title>');
